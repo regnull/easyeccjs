20b1200a1614f7c8ac34b69a3d8834e3df42b49d
Leonid Gorkin <regnull@gmail.com>
2022-12-28 20:42:19 -0500

    update key generation

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"crypto/sha256"
 	_ "crypto/sha512"
 	"fmt"
+	"strings"
 	"syscall/js"
 
 	"github.com/regnull/easyecc"
@@ -14,10 +16,18 @@ func main() {
 	<-done
 }
 
+// Hash256 does two rounds of SHA256 hashing.
+func Hash256(data []byte) []byte {
+	h := sha256.Sum256(data)
+	h1 := sha256.Sum256(h[:])
+	return h1[:]
+}
+
 func publicKeyFromPassword(this js.Value, args []js.Value) any {
 	password := args[0]
 	salt := args[1]
-	key := easyecc.NewPrivateKeyFromPassword([]byte(password.String()), []byte(salt.String()))
+	key := easyecc.NewPrivateKeyFromPassword([]byte(password.String()),
+		Hash256([]byte(strings.ToLower(salt.String()))))
 	keyHex := fmt.Sprintf("0x%x", key.PublicKey().SerializeCompressed())
 	return js.ValueOf(keyHex)
 }
